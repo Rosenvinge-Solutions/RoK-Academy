bea40f0b1a201a7cd6d724d8643501694b6e6204
Tobias Rosenvinge <TobiasRosenvinge@hotmail.com>
2022-10-14 23:39:56 +0200

    Added Feedback function - v3.5.0
    - Added a test to provide submission on the website, currently under test.

---
+++ ROKAcademy/WebApp/wwwroot/js/feedback.js
@@ -0,0 +1,26 @@
+﻿var dotnetRef = null;
+
+const handleFeedbackSubmit = (event) => {
+    event.preventDefault();
+
+    const feedbackForm = event.target;
+    const formData = new FormData(feedbackForm);
+
+    fetch("/", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: new URLSearchParams(formData).toString(),
+    })
+        .then(() => dotnetRef.invokeMethodAsync("FeedbackSubmittedSuccessfully"))
+        .catch((error) => alert(error));
+};
+
+export function setRef(ref) {
+    dotnetRef = ref;
+}
+
+export function addFeedbackEventListener() {
+    document
+        .querySelector("form#feedback")
+        .addEventListener("submit", handleFeedbackSubmit);
+}
