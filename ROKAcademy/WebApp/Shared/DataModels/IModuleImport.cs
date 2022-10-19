bf8cd7366bc87865cdfb3b531afd1ad479166d0a
Tobias Rosenvinge <TobiasRosenvinge@hotmail.com>
2022-10-19 19:27:59 +0200

    Import Improvements - v3.6.3
    - Improved the functionality of importing JavaScript into the backend.

---
+++ ROKAcademy/WebApp/Shared/DataModels/IModuleImport.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace WebApp.Shared.DataModels
+{
+    public interface IModuleImport
+    {
+        IJSRuntime? JSRuntime { get; }
+
+        Task<IJSObjectReference>? ImportModuleReferenceAsync(string module);
+    }
+}
---
+++ ROKAcademy/WebApp/Shared/DataModels/LayoutBase.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using WebApp.Shared.Extensions;
+
+namespace WebApp.Shared.DataModels;
+
+public abstract class LayoutBase : LayoutComponentBase, IModuleImport
+{
+    [Inject] public IJSRuntime? JSRuntime { get; init; }
+
+    public Task<IJSObjectReference> ImportModuleReferenceAsync(string module)
+    {
+        if (JSRuntime is null)
+        {
+            throw new ArgumentNullException("Couldn't import reference due to null reference.", nameof(JSRuntime));
+        }
+
+        return JSRuntime.InjectJsObjectReference("import", module);
+    }
+}
