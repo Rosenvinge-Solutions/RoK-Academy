30f84d6a9448644bbd381c49217b7368b23bf83d
Tobias Rosenvinge <TobiasRosenvinge@hotmail.com>
2023-03-11 21:00:25 +0100

    Feedback Submission Improvement

--- ROKAcademy/WebApp/Program.cs
+++ ROKAcademy/WebApp/Program.cs
@@ -1,11 +1,14 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using WebApp;
+using WebApp.Shared;
 using WebApp.Shared.Extensions;
+using WebApp.Shared.Modals;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.RootComponents.Add<FeedbackSubmissionModal>("#feedback-container");
 
 builder.Services
     .AddLogging()
---
+++ ROKAcademy/WebApp/Shared/Modals/FeedbackSubmissionModal.razor.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using System.Net.Http.Headers;
+using WebApp.Shared.DataModels;
+
+namespace WebApp.Shared.Modals
+{
+    public partial class FeedbackSubmissionModal
+    {
+        [Inject]
+        public HttpClient Client { get; set; }
+
+        [Inject]
+        public NavigationManager NavManager { get; set; }
+
+        private EditContext context;
+        private FeedbackFormModel model = new();
+        private bool feedbackSubmitSuccess = false;
+        private string feedbackSubmitMessage = string.Empty;
+
+        protected override void OnInitialized()
+        {
+            MakeContext();
+
+            base.OnInitialized();
+        }
+
+        private void MakeContext()
+        {
+            context = new(model);
+            context.EnableDataAnnotationsValidation();
+        }
+
+        private void CloseFeedback()
+        {
+            feedbackSubmitMessage = string.Empty;
+            feedbackSubmitSuccess = false;
+
+            MakeContext();
+        }
+
+        private async Task HandleValidFeedbackSubmitAsync()
+        {
+            feedbackSubmitMessage = string.Empty;
+
+            try
+            {
+                var encodedData = new FormUrlEncodedContent(model.FormData);
+
+                var request = new HttpRequestMessage(HttpMethod.Post, requestUri: string.Empty);
+                request.Content = encodedData;
+                request.Content.Headers.ContentType = new MediaTypeHeaderValue("application/x-www-form-urlencoded");
+
+                var response = await Client.SendAsync(request);
+
+                feedbackSubmitSuccess = response.IsSuccessStatusCode;
+
+                if (feedbackSubmitSuccess)
+                {
+                    feedbackSubmitMessage = "Your request was sent successfully.";
+                    return;
+                }
+
+                feedbackSubmitMessage = "Something went wrong while making the feedback request.";
+            }
+            finally
+            {
+                context.MarkAsUnmodified();
+                model = new();
+            }
+        }
+    }
+}
